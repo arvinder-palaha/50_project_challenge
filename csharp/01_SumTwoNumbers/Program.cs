e2780c5176f73cfc8406f1a0c01f6c488612c291
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-15 20:26:42 +0000

    hardcoded sum of two numbers

--- csharp/01_SumTwoNumbers/Program.cs
+++ csharp/01_SumTwoNumbers/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿float number1 = 1.5f;
+float number2 = 2.5f;
+float sum = number1 + number2;
+Console.WriteLine("The sum of " + number1 + " and " + number2 + " is " + sum);
