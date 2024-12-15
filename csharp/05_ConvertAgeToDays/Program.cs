7a651f0ccb81ea1daa7952519413c97af3f9ab68
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-15 23:42:29 +0000

    Got it working with one positional argument

--- csharp/05_ConvertAgeToDays/Program.cs
+++ csharp/05_ConvertAgeToDays/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿float? years = null;
+
+ParseArguments(args, ref years);
+
+Console.WriteLine($"{years} years is {CalculateDays(years!.Value)} days.");
+
+static float CalculateDays(float years)
+{
+    return years * 365;
+}
+
+static void ParseArguments(string[] args, ref float? years)
+{
+    if (args.Length != 1)
+    {
+        PrintUsage();
+    }
+
+    try
+    {
+        years = float.Parse(args[0]);
+    }
+    catch (FormatException)
+    {
+        PrintUsage();
+    }
+}
+
+static void PrintUsage()
+{
+    Console.WriteLine("Usage: dotnet run <age>");
+    Environment.Exit(1);
+}
