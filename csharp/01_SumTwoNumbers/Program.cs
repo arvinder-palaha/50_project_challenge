189c3a1b51b7b0224ac1b521f9c59aa447cbb6b6
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-15 20:37:30 +0000

    created function for sum

--- csharp/01_SumTwoNumbers/Program.cs
+++ csharp/01_SumTwoNumbers/Program.cs
@@ -1,4 +1,9 @@
 ﻿float number1 = 1.5f;
 float number2 = 2.5f;
-float sum = number1 + number2;
+float sum = Sum(number1, number2);
 Console.WriteLine("The sum of " + number1 + " and " + number2 + " is " + sum);
+
+static float Sum(float number1, float number2)
+{
+    return number1 + number2;
+}
