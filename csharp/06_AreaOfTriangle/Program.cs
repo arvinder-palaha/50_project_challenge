08629ca2077590cb4fd52a4dd9b418dfa1f52c07
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-16 08:28:23 +0000

    error handling

--- csharp/06_AreaOfTriangle/Program.cs
+++ csharp/06_AreaOfTriangle/Program.cs
@@ -6,6 +6,10 @@
         float? input_height = null;
 
         ParseArguments(args, ref input_base, ref input_height);
+
+        float area = CalculateArea(input_base, input_height);
+
+        Console.WriteLine($"{area}");
     }
 
     private static void ParseArguments(string[] args, ref float? input_base, ref float? input_height)
@@ -15,15 +19,20 @@
             PrintUsage();
         }
 
-        input_base = float.Parse(args[0]);
-        input_height = float.Parse(args[1]);
-
-        Console.WriteLine($"{input_base} {input_height}");
+        try
+        {
+            input_base = float.Parse(args[0]);
+            input_height = float.Parse(args[1]);
+        }
+        catch (FormatException)
+        {
+            PrintUsage();
+        }
     }
 
-    private static float CalculateArea(float input_base, float input_height)
+    private static float CalculateArea(float? input_base, float? input_height)
     {
-        return (input_base * input_height) / 2;
+        return input_base!.Value * input_height!.Value / 2;
     }
 
     private static void PrintUsage()
--- csharp/06_AreaOfTriangle/readme.md
+++ csharp/06_AreaOfTriangle/readme.md
@@ -5,6 +5,6 @@ From the base and height of a triangle, calculate the area.
 ## Goals
 
 - [x] Callable from command line
-- [ ] Will accept two positional arguments
-- [ ] Will return the area of the triangle
-- [ ] Will handle errors (show usage)
+- [x] Will accept two positional arguments
+- [x] Will return the area of the triangle
+- [x] Will handle errors (show usage)
