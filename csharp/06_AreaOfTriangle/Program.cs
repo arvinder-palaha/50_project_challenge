77d3b2d81c5309c6d7298fea6086e9546b0d451a
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-16 08:24:12 +0000

    Arguments are actually parsed. Calculate area method.

--- csharp/06_AreaOfTriangle/Program.cs
+++ csharp/06_AreaOfTriangle/Program.cs
@@ -15,11 +15,20 @@
             PrintUsage();
         }
 
+        input_base = float.Parse(args[0]);
+        input_height = float.Parse(args[1]);
+
         Console.WriteLine($"{input_base} {input_height}");
+    }
+
+    private static float CalculateArea(float input_base, float input_height)
+    {
+        return (input_base * input_height) / 2;
     }
 
     private static void PrintUsage()
     {
         Console.WriteLine("Usage: dotnet run <base> <height>");
+        Environment.Exit(1);
     }
 }
