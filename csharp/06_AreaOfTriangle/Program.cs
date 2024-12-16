d796c607aa9781f4c0f6afe1123d323993093cf4
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-16 08:21:44 +0000

    print usage and parse arguments, should be private as only main will call them

--- csharp/06_AreaOfTriangle/Program.cs
+++ csharp/06_AreaOfTriangle/Program.cs
@@ -2,6 +2,24 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        float? input_base = null;
+        float? input_height = null;
+
+        ParseArguments(args, ref input_base, ref input_height);
+    }
+
+    private static void ParseArguments(string[] args, ref float? input_base, ref float? input_height)
+    {
+        if (args.Length != 2)
+        {
+            PrintUsage();
+        }
+
+        Console.WriteLine($"{input_base} {input_height}");
+    }
+
+    private static void PrintUsage()
+    {
+        Console.WriteLine("Usage: dotnet run <base> <height>");
     }
 }
