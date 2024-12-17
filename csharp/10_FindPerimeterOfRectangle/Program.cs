d6632eae22af82152d00cfb8945a7596223a8519
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-17 22:22:43 +0000

    print usage if not correct number of arguments

--- csharp/10_FindPerimeterOfRectangle/Program.cs
+++ csharp/10_FindPerimeterOfRectangle/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Length != 2)
+{
+    PrintUsage();
+}
+
+static void PrintUsage()
+{
+    Console.WriteLine("Usage: Program.exe <length> <width>");
+    Environment.Exit(1);
+}
