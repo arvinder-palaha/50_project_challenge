740453c1c6156df56dbe1c85938536a940da3de9
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-18 23:13:48 +0000

    finished hamming distance

--- csharp/14_HammingDistance/Program.cs
+++ csharp/14_HammingDistance/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Length != 2)
+{
+    PrintUsage();
+}
+
+string str1 = args[0];
+string str2 = args[1];
+
+if (str1.Length != str2.Length)
+{
+    PrintUsage();
+}
+
+int hammingDistance = CalculateHammingDistance(str1, str2);
+Console.WriteLine(hammingDistance);
+
+static void PrintUsage()
+{
+    Console.WriteLine("Usage: dotnet run <string1> <string2>");
+    Environment.Exit(1);
+}
+
+static int CalculateHammingDistance(string str1, string str2)
+{
+    int result = 0;
+    for (int i = 0; i < str1.Length; i++)
+    {
+        if (str1[i] != str2[i])
+        {
+            result++;
+        }
+    }
+    return result;
+}
