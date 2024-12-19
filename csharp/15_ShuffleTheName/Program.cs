287766b3a993104066e5bbdbe54dca7e9a91cc6b
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-19 23:41:00 +0000

    project 15 Finished

---
+++ csharp/15_ShuffleTheName/Program.cs
@@ -0,0 +1,17 @@
+﻿if (args.Length != 1)
+{
+    PrintUsage();
+}
+
+string name = args[0];
+string[] nameParts = name.Split(' ');
+string firstName = nameParts[0];
+string lastName = nameParts[1];
+
+Console.WriteLine($"{lastName} {firstName}");
+
+static void PrintUsage()
+{
+    Console.WriteLine("Usage: dotnet run \"<name>\"");
+    Environment.Exit(1);
+}
---
+++ csharp/15_ShuffleTheName/readme.md
@@ -0,0 +1,11 @@
+# Shuffle The Name
+
+Take a first name and last name as arguments.
+Return a string with the first name and last name reversed.
+There will be a space between the first name and last name.
+
+## Examples
+
+ShuffleTheName("John Doe") ➞ "Doe John"
+ShuffleTheName("Jane Smith") ➞ "Smith Jane"
+ShuffleTheName("Alice Johnson") ➞ "Johnson Alice"
