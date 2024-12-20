b0d46f75fe0c3622e68b5e50048b9fe504597de0
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-20 00:03:16 +0000

    Smaller String Number done

---
+++ csharp/16_SmallerStringNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿if (args.Length != 2)
+{
+    PrintUsage();
+}
+
+try
+{
+    int number1 = int.Parse(args[0]);
+    int number2 = int.Parse(args[1]);
+
+    int smallerNumber = GetSmallerNumber(number1, number2);
+
+    Console.WriteLine(smallerNumber);
+}
+catch (FormatException)
+{
+    PrintUsage();
+}
+
+private static int GetSmallerNumber(int number1, int number2)
+{
+    return number1 < number2 ? number1 : number2;
+}
+
+static void PrintUsage()
+{
+    Console.WriteLine("Usage: smallerNum <number1> <number2>");
+    Environment.Exit(1);
+}
+
---
+++ csharp/16_SmallerStringNumber/readme.md
@@ -0,0 +1,12 @@
+# 16. Smaller String Number
+
+Return the smaller number from two strings
+
+## Example
+
+smallerNum("21", "44") ➞ "21"
+
+smallerNum("1500", "1") ➞ "1"
+
+smallerNum("5", "5") ➞ "5"
+
