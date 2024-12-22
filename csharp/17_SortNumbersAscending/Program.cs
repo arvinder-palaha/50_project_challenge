671a16a6411a53843abea7b4cd989f926e7b6e1f
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-22 17:12:58 +0000

    Completed sort ascending

---
+++ csharp/17_SortNumbersAscending/Program.cs
@@ -0,0 +1,25 @@
+﻿static int[] SortNumbersAscending(int[]? numbers)
+{
+    int[] result = numbers ?? [];
+
+    Array.Sort(result);
+
+    return result;
+}
+
+static void PrintNumbers(int[] numbers)
+{
+    Console.WriteLine(string.Join(", ", numbers));
+}
+
+int[] input1 = [1, 2, 10, 50, 5];
+int[] input2 = [80, 29, 4, -95, -24, 85];
+int[]? input3 = null;
+int[] input4 = [];
+
+PrintNumbers(SortNumbersAscending(input1));
+PrintNumbers(SortNumbersAscending(input2));
+PrintNumbers(SortNumbersAscending(input3));
+PrintNumbers(SortNumbersAscending(input4));
+
+
---
+++ csharp/17_SortNumbersAscending/readme.md
@@ -0,0 +1,13 @@
+# sort numbers ascending
+
+Take an array of numbers, and return a new array with the numbers sorted from smallest to largest.
+
+## Examples
+
+SortNumsAscending([1, 2, 10, 50, 5]) ➞ [1, 2, 5, 10, 50]
+
+SortNumsAscending([80, 29, 4, -95, -24, 85]) ➞ [-95, -24, 4, 29, 80, 85]
+
+SortNumsAscending(null) ➞ []
+
+SortNumsAscending([]) ➞ []
