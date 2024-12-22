fa88464c8f7ff92822a29f9c2c720386013e3459
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-22 17:26:43 +0000

    playing with own sort version, needs either multiple passes or better re-arranging strategy

--- csharp/17_SortNumbersAscending/Program.cs
+++ csharp/17_SortNumbersAscending/Program.cs
@@ -7,6 +7,26 @@
     return result;
 }
 
+static int[] SNAarv(int[] numbers)
+{
+    PrintNumbers(numbers);
+    int lnum = numbers.Length;
+    int temp;
+
+    for (int i = 0; i < lnum; i++)
+    {
+        if (i<lnum-1) {
+            if (numbers[i] > numbers[i+1]) {
+                temp = numbers[i];
+                numbers[i] = numbers[i+1];
+                numbers[i+1] = temp;
+            }
+        }
+    }
+    PrintNumbers(numbers);
+    return numbers;
+}
+
 static void PrintNumbers(int[] numbers)
 {
     Console.WriteLine(string.Join(", ", numbers));
@@ -16,6 +36,9 @@ int[] input1 = [1, 2, 10, 50, 5];
 int[] input2 = [80, 29, 4, -95, -24, 85];
 int[]? input3 = null;
 int[] input4 = [];
+
+SNAarv(input1);
+SNAarv(input2);
 
 PrintNumbers(SortNumbersAscending(input1));
 PrintNumbers(SortNumbersAscending(input2));
