85067ed7c490149ace7d444174e01747469af415
Arvinder Palaha <arvinder.palaha@googlemail.com>
2024-12-26 13:53:52 +0000

    Completed hacker speak

--- csharp/18_HackerSpeak/Program.cs
+++ csharp/18_HackerSpeak/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿if (args.Length != 1)
+{
+    PrintUsage();
+}
+
+var input = args[0];
+var hackerSpeak = ConvertToHackerSpeak(input);
+
+Console.WriteLine(hackerSpeak);
+
+static void PrintUsage()
+{
+    Console.WriteLine("Usage: dotnet run \"<string>\"");
+    Environment.Exit(1);
+}
+
+static string ConvertToHackerSpeak(string input)
+{
+    return input.Replace("a", "4").Replace("e", "3").Replace("i", "1").Replace("o", "0").Replace("s", "5");
+}
+
