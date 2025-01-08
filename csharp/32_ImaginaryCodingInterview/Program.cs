367e56e520db401d670ec27d994cfeee5bbf2f92
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-01-08 22:26:11 +0000

    smidge of refactoring, used an enum

--- csharp/32_ImaginaryCodingInterview/Program.cs
+++ csharp/32_ImaginaryCodingInterview/Program.cs
@@ -8,6 +8,18 @@ public class Program
     private const int MediumQuestionTimeLimit = 15;
     private const int HardQuestionTimeLimit = 20;
 
+    private enum QuestionDifficulty
+    {
+        VeryEasy1 = 0,
+        VeryEasy2 = 1,
+        Easy1 = 2,
+        Easy2 = 3,
+        Medium1 = 4,
+        Medium2 = 5,
+        Hard1 = 6,
+        Hard2 = 7
+    }
+
     public static void Main(string[] args){}
 
     public static string Interview(int[] times, int totalTime)
@@ -18,16 +30,16 @@ public class Program
         if (times.Length != 8)
             return "disqualified";
 
-        if (times[0] > VeryEasyQuestionTimeLimit || times[1] > VeryEasyQuestionTimeLimit)
+        if (times[(int)QuestionDifficulty.VeryEasy1] > VeryEasyQuestionTimeLimit || times[(int)QuestionDifficulty.VeryEasy2] > VeryEasyQuestionTimeLimit)
             return "disqualified";
 
-        if (times[2] > EasyQuestionTimeLimit || times[3] > EasyQuestionTimeLimit)
+        if (times[(int)QuestionDifficulty.Easy1] > EasyQuestionTimeLimit || times[(int)QuestionDifficulty.Easy2] > EasyQuestionTimeLimit)
             return "disqualified";
 
-        if (times[4] > MediumQuestionTimeLimit || times[5] > MediumQuestionTimeLimit)
+        if (times[(int)QuestionDifficulty.Medium1] > MediumQuestionTimeLimit || times[(int)QuestionDifficulty.Medium2] > MediumQuestionTimeLimit)
             return "disqualified";
 
-        if (times[6] > HardQuestionTimeLimit || times[7] > HardQuestionTimeLimit)
+        if (times[(int)QuestionDifficulty.Hard1] > HardQuestionTimeLimit || times[(int)QuestionDifficulty.Hard2] > HardQuestionTimeLimit)
             return "disqualified";
 
         return "qualified";
