896699c8ce7f6bc76b6f5ecb7cb04ea950e50cfa
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-01-08 21:36:05 +0000

    Test for loaded die

--- csharp/Tests/31_IsTheDieLoadedTests.cs
+++ csharp/Tests/31_IsTheDieLoadedTests.cs
@@ -2,10 +2,13 @@ namespace Tests;
 
 public class IsTheDieLoadedTests
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(new int[] { 44, 52, 33, 40, 41, 30 }, true)]
+    [InlineData(new int[] { 34, 27, 23, 20, 32, 28 }, true)]
+    [InlineData(new int[] { 10, 20, 11, 5, 19, 16 }, false)]
+    public void Test1(int[] rolls, bool expected)
     {
-        Assert.Equal(true, false);
+        Assert.Equal(expected, IsTheDieLoaded.Program.FairDie(rolls));
     }
 }
 
