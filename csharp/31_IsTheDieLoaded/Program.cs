77307fc8a0e2fa3d75cb7167b06e480e4f054308
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-01-08 21:39:40 +0000

    Algorithm sketch

--- csharp/31_IsTheDieLoaded/Program.cs
+++ csharp/31_IsTheDieLoaded/Program.cs
@@ -2,13 +2,14 @@
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+    public static void Main(string[] args){}
 
     public static bool FairDie(int[] rolls)
     {
+        /// work out expected number of rolls
+        /// work out chi squared
+        /// compare chi squared to critical value
+        /// return true if chi squared is less than critical value, false otherwise
         throw new NotImplementedException();
     }
 }
