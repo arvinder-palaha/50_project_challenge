0389a07c1ccdc43df18d481e777240b3dac639c6
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-01-08 20:27:34 +0000

    plan the function

--- csharp/30_CountOnesInBinaryRepOfInteger/Program.cs
+++ csharp/30_CountOnesInBinaryRepOfInteger/Program.cs
@@ -6,6 +6,9 @@ public class Program
 
     public static int CountOnesInBinaryRepOfInteger(int number)
     {
+        /// convert number to binary string
+        /// count the number of 1s in the string
+        /// return the count
         return 0;
     }
 }
