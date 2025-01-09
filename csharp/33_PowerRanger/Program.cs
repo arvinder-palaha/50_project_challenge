084600b61e21945807c75ff5454f162aebcfabd9
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-01-09 20:30:00 +0000

    Finished project 33

--- csharp/33_PowerRanger/Program.cs
+++ csharp/33_PowerRanger/Program.cs
@@ -4,11 +4,31 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        if (args.Length == 3)
+        {
+            int n = int.Parse(args[0]);
+            int a = int.Parse(args[1]);
+            int b = int.Parse(args[2]);
+            Console.WriteLine(PowerRanger(n, a, b));
+        }
+        else
+        {
+            Console.WriteLine("Please provide three arguments: n, a, b");
+        }
     }
 
     public static int PowerRanger(int n, int a, int b)
     {
-        throw new NotImplementedException();
+        int count = 0;
+        for (int i = 1; i <= b; i++)
+        {
+            int raised = (int)Math.Pow(i, n);
+            Console.WriteLine($"{raised} {a} {b}");
+            if (raised >= a && raised <= b)
+                count++;
+            if (raised > b)
+                break;
+        }
+        return count;
     }
 }
