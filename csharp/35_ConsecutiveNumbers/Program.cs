c6e1c492a4e38548b6790a8f77f438f2faebc7a8
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-01-24 13:25:13 +0000

    Finished consecutive numbers

--- csharp/35_ConsecutiveNumbers/Program.cs
+++ csharp/35_ConsecutiveNumbers/Program.cs
@@ -4,14 +4,22 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(Cons(new int[] { 5, 1, 4, 3, 2 }));
     }
 
     public static bool Cons(int[] arr)
     {
         // first, sort the array 
+        Array.Sort(arr);
+
         // then, check if the difference between each element is 1
-        // if it is, return true, otherwise return false
-        throw new NotImplementedException();
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i + 1] - arr[i] != 1)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
