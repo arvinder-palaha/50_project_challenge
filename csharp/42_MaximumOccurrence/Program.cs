68ea5ed1c582aba1c0d266111e3d18c20c1aa71d
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-03-25 23:01:07 +0000

    completed project 42

--- csharp/42_MaximumOccurrence/Program.cs
+++ csharp/42_MaximumOccurrence/Program.cs
@@ -9,6 +9,30 @@ public class Program
 
     public static string MaxOccur(string text)
     {
-        throw new NotImplementedException();
+        var charCounts = new Dictionary<char, int>();
+        foreach (var c in text)
+        {
+            if (charCounts.ContainsKey(c))
+            {
+                charCounts[c]++;
+            }
+            else
+            {
+                charCounts[c] = 1;
+            }
+        }
+
+        var maxOccurrences = charCounts.Values.Max();
+        var results = charCounts.Where(kpv => kpv.Value == maxOccurrences)
+            .Select(kvp => kvp.Key)
+            .OrderBy(c => c)
+            .ToArray();
+
+        if (maxOccurrences < 2)
+        {
+            return "No Repetition";
+        }
+
+        return results.Length > 1 ? string.Join(", ", results) : results[0].ToString();
     }
 }
