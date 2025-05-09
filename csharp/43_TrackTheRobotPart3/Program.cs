902422378c8c075cbfa4ea5086a4a2a9cbb3ccfb
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-05-09 23:59:20 +0100

    completed track the robot

--- csharp/43_TrackTheRobotPart3/Program.cs
+++ csharp/43_TrackTheRobotPart3/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
+﻿using System;
 
 namespace TrackTheRobotPart3;
 
@@ -11,12 +10,20 @@ public class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Track The Robot Part 3");
+        if (args.Length == 1)
+        {
+            Console.WriteLine(TrackRobotPart3(args[0]));
+        }
     }
 
     public static int[] TrackRobotPart3(string instructions)
     {
         int[] position = [0, 0];
         int[] direction = [1, 0];
+
+        Console.WriteLine($"Initial position: {position[X]}, {position[Y]}");
+        Console.WriteLine($"Initial direction: {direction[X]}, {direction[Y]}");
+        Console.WriteLine("Instructions: " + instructions);
 
         foreach (char instruction in instructions)
         {
@@ -39,21 +46,30 @@ public class Program
 
     private static int[] MoveForwards( int[] position, int[] direction)
     {
-        return [position[X] + direction[X], position[Y] + direction[Y]];
+        int[] result = [position[X] + direction[X], position[Y] + direction[Y]];
+        Console.WriteLine($"Moved forwards to: {result[X]}, {result[Y]}");
+        return [result[X], result[Y]];
     }
 
     private static int[] TurnLeft(int[] direction)
     {
-        // up -> left
-        // left -> down
-        // down -> right
-        // right -> up
-        // return [direction[Y], -direction[X]];
-        return direction;
+        // north -> west is [0,1] -> [-1,0]
+        // west -> south is [-1,0] -> [0,-1]
+        // south -> east is [0,-1] -> [1,0]
+        // east -> north is [1,0] -> [0,1]
+        int[] result = [-direction[Y], direction[X]];
+        Console.WriteLine($"Turned left to: {result[X]}, {result[Y]}");
+        return [result[X], result[Y]];
     }
 
     private static int[] TurnRight(int[] direction)
     {
-        return direction;
+        // north -> east is [0,1] -> [1,0]
+        // east -> south is [1,0] -> [0,-1]
+        // south -> west is [0,-1] -> [-1,0]
+        // west -> north is [-1,0] -> [0,1]
+        int[] result = [direction[Y], -direction[X]];
+        Console.WriteLine($"Turned right to: {result[X]}, {result[Y]}");
+        return [result[X], result[Y]];
     }
 }
