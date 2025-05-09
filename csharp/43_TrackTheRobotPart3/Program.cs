5bd055690ba36c9e92584ac8b3db223c277ab4aa
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-05-09 23:20:20 +0100

    draft of program

--- csharp/43_TrackTheRobotPart3/Program.cs
+++ csharp/43_TrackTheRobotPart3/Program.cs
@@ -1,7 +1,13 @@
-﻿namespace TrackTheRobotPart3;
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TrackTheRobotPart3;
 
 public class Program
 {
+    private const int X = 0;
+    private const int Y = 1;
+
     public static void Main(string[] args)
     {
         Console.WriteLine("Track The Robot Part 3");
@@ -9,7 +15,45 @@ public class Program
 
     public static int[] TrackRobotPart3(string instructions)
     {
-        int[] position = { 0, 0 };
+        int[] position = [0, 0];
+        int[] direction = [1, 0];
+
+        foreach (char instruction in instructions)
+        {
+            if (instruction == '.')
+            {
+                position = MoveForwards(position, direction);
+            }
+            else if (instruction == '<')
+            {
+                direction = TurnLeft(direction);
+            }
+            else if (instruction == '>')
+            {
+                direction = TurnRight(direction);
+            }
+        }
+
         return position;
+    }
+
+    private static int[] MoveForwards( int[] position, int[] direction)
+    {
+        return [position[X] + direction[X], position[Y] + direction[Y]];
+    }
+
+    private static int[] TurnLeft(int[] direction)
+    {
+        // up -> left
+        // left -> down
+        // down -> right
+        // right -> up
+        // return [direction[Y], -direction[X]];
+        return direction;
+    }
+
+    private static int[] TurnRight(int[] direction)
+    {
+        return direction;
     }
 }
