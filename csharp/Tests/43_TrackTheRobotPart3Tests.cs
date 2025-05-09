f3fa3b329d19465828962b62e8969f61842a17a9
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-05-09 23:59:11 +0100

    tests were wrong, manually checked em

--- csharp/Tests/43_TrackTheRobotPart3Tests.cs
+++ csharp/Tests/43_TrackTheRobotPart3Tests.cs
@@ -6,10 +6,10 @@ public class TrackTheRobotPart3Tests
 {
     [Theory]
     [InlineData("..<.<.", new int[] { 1, 1 })]
-    [InlineData(">.<.<.", new int[] { -1, 1 })]
-    [InlineData(">.<.<.<.", new int[] { -2, 1 })]
-    [InlineData(">.<.<.<.<.", new int[] { -3, 1 })]
-    [InlineData(">.<.<.<.<.<.", new int[] { -4, 1 })]
+    [InlineData(">.<.<.", new int[] { 1, 0 })]
+    [InlineData(">.<.<.<.", new int[] { 0, 0 })]
+    [InlineData(">.<.<.<.<.", new int[] { 0, -1 })]
+    [InlineData(">.<.<.<.<.<.", new int[] { 1, -1 })]
     public void TestTrackRobotPart3(string instructions, int[] expected)
     {
         Assert.Equal(expected, Program.TrackRobotPart3(instructions));
