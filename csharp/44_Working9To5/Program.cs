f9b857a1332664bfadae1915f89b61c2b0ecc96c
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-05-22 23:58:41 +0100

    solved 9 to 5

--- csharp/44_Working9To5/Program.cs
+++ csharp/44_Working9To5/Program.cs
@@ -9,6 +9,19 @@ public class Program
 
     public static double Working9To5(double startHour, double endHour, double hourlyRate, double overtimeMultiplier)
     {
-        return 0;
+        double result = 0;
+        // how many hours before 9
+        double hourseBefore9 = Math.Max(0, 9 - startHour);
+        result += hourseBefore9 * hourlyRate * overtimeMultiplier;
+
+        // how many hours after 5
+        double hoursAfter5 = Math.Max(0, endHour - 17);
+        result += hoursAfter5 * hourlyRate * overtimeMultiplier;
+
+        // how many hours between 9 and 5
+        double hoursBetween9and5 = endHour - startHour - hourseBefore9 - hoursAfter5;
+        result += hoursBetween9and5 * hourlyRate;
+
+        return result;
     }
 }
