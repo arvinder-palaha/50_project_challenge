c545c0a79ef42d46250cc47b99946f19ba74b7cb
Arvinder Palaha <arvinder.palaha@googlemail.com>
2025-05-24 09:01:45 +0100

    draft and notes

--- csharp/45_KnightsOnABoard/Program.cs
+++ csharp/45_KnightsOnABoard/Program.cs
@@ -14,5 +14,40 @@ public class Program
     public static bool CannotCapture(int[,] board)
     {
         throw new NotImplementedException();
+        // get size of board
+        // iterate through board
+        // if knight is found
+        // check all possible moves
+        // if a knight is found in the possible moves, return false
+        // if no knight is found in the possible moves, return true
     }
 }
+
+/*
+Possible moves of a knight:
+
+0 0 0 0 0 0 0 0
+0 0 / 0 / 0 0 0
+0 / 0 0 0 / 0 0
+0 0 0 K 0 0 0 0
+0 / 0 0 0 / 0 0
+0 0 / 0 / 0 0 0
+0 0 0 0 0 0 0 0
+0 0 0 0 0 0 0 0
+
+k-position: (3, 3)
+
+possible moves:
+(1, 2)
+(1, 4)
+(2, 1)
+(2, 5)
+(4, 1)
+(4, 5)
+(5, 2)
+(5, 4)
+
+
+
+
+*/
