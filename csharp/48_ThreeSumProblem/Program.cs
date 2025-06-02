30ee4a1d8e4ac0300eed7610bc5f5a1ec133a205
Arvinder Palaha <Arvinder.Palaha@WilliamsF1.com>
2025-06-02 10:21:52 +0100

    sketch of algorithm

--- csharp/48_ThreeSumProblem/Program.cs
+++ csharp/48_ThreeSumProblem/Program.cs
@@ -13,6 +13,13 @@ public class Program
 
     public static string ThreeSum(int[] nums)
     {
+        // fidn all triplets that sum to 0
+        // are there at least 3 elements? -> return empty array
+        // loop through the array using loop index as the first element of a possible 0-sum triplet
+        // loop through the array after the first element, using loop index as the second element of a possible 0-sum triplet
+        // loop through the array after the second element, using loop index as the third element of a possible 0-sum triplet
+        // if the sum of the three elements is 0, add the triplet to the result
+        // return the result
         return "{ }";
     }
 }
