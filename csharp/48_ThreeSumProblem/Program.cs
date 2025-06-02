dc3dc7e3aa18de56589702152ae29d1fc04aff75
Arvinder Palaha <Arvinder.Palaha@WilliamsF1.com>
2025-06-02 13:42:14 +0100

    Completed Three Sum Problem

--- csharp/48_ThreeSumProblem/Program.cs
+++ csharp/48_ThreeSumProblem/Program.cs
@@ -16,23 +16,45 @@ public class Program
     public static string ThreeSum(int[] nums)
     {
         // find all triplets that sum to 0
-        string result = "{ ";
+        var triplets = new HashSet<int[]>();
 
         // are there at least 3 elements? -> return empty array
         if (nums.Length < 3)
         {
-            return result += "}";
+            return "{ }";
         }
 
         // loop through the array using loop index as the first element of a possible 0-sum triplet
         for (int first = 0; first < nums.Length -2; first ++)
         {
             // loop through the array after the first element, using loop index as the second element of a possible 0-sum triplet
-            // loop through the array after the second element, using loop index as the third element of a possible 0-sum triplet
-            // if the sum of the three elements is 0, add the triplet to the result
+            for (int second = first + 1; second < nums.Length - 1; second ++)
+            {
+                // loop through the array after the second element, using loop index as the third element of a possible 0-sum triplet
+                for (int third = second + 1; third < nums.Length; third ++)
+                {
+                    // if the sum of the three elements is 0, add the triplet to the result
+                    Console.WriteLine($"{nums[first]}, {nums[second]}, {nums[third]}");
+                    var triplet = new int[] { nums[first], nums[second], nums[third] };
+                    if (triplet.Sum() == 0)
+                    {
+                        if (!CheckIfTripletExists(triplets, triplet))
+                        {
+                            triplets.Add(triplet);
+                        }
+                    }
+                }
+            }
         }
 
         // return the result
-        return result + "}";
+        return triplets.Any()
+            ? "{ " + string.Join(", ", triplets.Select(t => $"{{ {string.Join(", ", t)} }}")) + " }"
+            : "{ }";
+    }
+
+    private static bool CheckIfTripletExists(HashSet<int[]> triplets, int[] triplet)
+    {
+        return triplets.Any(t => t.SequenceEqual(triplet));
     }
 }
--- csharp/Tests/48_ThreeSumProblemTests.cs
+++ csharp/Tests/48_ThreeSumProblemTests.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace Tests;
 
 using ThreeSumProblem;
@@ -6,12 +8,12 @@ public class ThreeSumProblemTests
 {
     [Theory]
     [InlineData(new int[] { 0, 1, -1, -1, 2 }, "{ { 0, 1, -1 }, { -1, -1, 2 } }")]
-    [InlineData(new int[] { 0, 0, 0, 5, -5 }, "{ { 0, 0, 0 }, { 0, 5, -5} }")]
+    [InlineData(new int[] { 0, 0, 0, 5, -5 }, "{ { 0, 0, 0 }, { 0, 5, -5 } }")]
     [InlineData(new int[] { 1, 2, 3 }, "{ }")]
     [InlineData(new int[] { 1 }, "{ }")]
     public void Test1(int[] nums, string expected)
     {
         var result = Program.ThreeSum(nums);
-        Assert.Equal(expected, result);
+        result.Should().Be(expected);
     }
 }
