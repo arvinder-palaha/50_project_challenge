8b61303611b56ee40c82b45902a41a219e93cd09
Arvinder Palaha <Arvinder.Palaha@WilliamsF1.com>
2025-06-02 13:52:15 +0100

    setup for challenge 49

---
+++ csharp/49_PointWithATriangle/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PointWithATriangle;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Point with a Triangle");
+        if (args.Length > 0)
+        {
+            Console.WriteLine(WithinTriangle(args[0].Split(',').Select(int.Parse).ToArray()));
+        }
+    }
+    
+    public static bool WithinTriangle(int[] points)
+    {
+        return false;
+    }
+}
