eb13a92ca3bb2619a84a8cb8d9dfdb09cd4c570b
Arvinder Palaha <Arvinder.Palaha@WilliamsF1.com>
2025-06-04 09:56:08 +0100

    setup for challenge 50: maxie and minnie

---
+++ csharp/50_MaxieAndMinnie/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MaxieAndMinnie;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Maxie and Minnie");
+        if (args.Length > 0)
+        {
+            Console.WriteLine(MaxieAndMinnie(long.Parse(args[0])));
+        }
+    }
+    
+    public static long[] MaxieAndMinnie(long num)
+    {
+        return [num, num];
+    }
+}
