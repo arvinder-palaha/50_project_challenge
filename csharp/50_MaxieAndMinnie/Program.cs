c318ad902699b9e9e483cbe3735be5338d7c814f
Arvinder Palaha <Arvinder.Palaha@WilliamsF1.com>
2025-06-10 09:28:59 +0100

    algorithm sketch

--- csharp/50_MaxieAndMinnie/Program.cs
+++ csharp/50_MaxieAndMinnie/Program.cs
@@ -13,6 +13,12 @@ public class Program
     
     public static long[] MaxieAndMinnie(long num)
     {
+        // turn the number into a string, or array of digits
+        // count the number of digits
+        // hold a minimin value and a maximum value
+        // loop through each digit, swap it with another digit
+        // make a long from the digits
+        // check for min and max
         return [num, num];
     }
 }
