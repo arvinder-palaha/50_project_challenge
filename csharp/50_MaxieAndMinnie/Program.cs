a5dea7f7d230f04b66771d3aab0a149aae7fd9c0
Arvinder Palaha <Arvinder.Palaha@WilliamsF1.com>
2025-06-10 09:38:45 +0100

    implemented algo, got 2/4 tests passed

--- csharp/50_MaxieAndMinnie/Program.cs
+++ csharp/50_MaxieAndMinnie/Program.cs
@@ -14,11 +14,44 @@ public class Program
     public static long[] MaxieAndMinnie(long num)
     {
         // turn the number into a string, or array of digits
+        var digits = num.ToString().ToCharArray();
+
         // count the number of digits
+        var digitCount = digits.Length;
+
         // hold a minimin value and a maximum value
+        var minnie = num;
+        var maxie = num;
+
         // loop through each digit, swap it with another digit
-        // make a long from the digits
-        // check for min and max
-        return [num, num];
+        for (int i = 0; i < digitCount; i++)
+        {
+            for (int j = i + 1; j < digitCount; j++)
+            {
+                // swap the digits
+                var swapped = SwapDigits(digits, i, j);
+
+                // check for min and max
+                if (swapped < minnie)
+                {
+                    minnie = swapped;
+                }
+                if (swapped > maxie)
+                {
+                    maxie = swapped;
+                }
+            }
+        }
+
+        return [maxie, minnie];
+    }
+
+    private static long SwapDigits(char[] digits, int i, int j)
+    {
+        var result = (char[])digits.Clone();
+        var temp = result[i];
+        result[i] = result[j];
+        result[j] = temp;
+        return long.Parse(new string(result));
     }
 }
